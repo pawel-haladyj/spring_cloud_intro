99b7e623143fadca48a21b8c158e0818849e74c0
Pawel Haladyj <pawel@haladyj.pl>
2020-03-31 13:21:09 +0200

    initial commit

---
+++ spring-cloud-intro/src/main/java/pl/haladyj/springcloudintro/SpringCloudIntroApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springcloudintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudIntroApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-intro/src/test/java/pl/haladyj/springcloudintro/SpringCloudIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springcloudintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
