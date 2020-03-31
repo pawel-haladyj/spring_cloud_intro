c3a9e14faca8002c6c139307d82f7d714f347575
Pawel Haladyj <pawel@haladyj.pl>
2020-03-31 13:31:21 +0200

    established catalog, data, info services

---
+++ movie-data-service/src/test/java/pl/haladyj/moviedataservice/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.moviedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ movie-info-service/src/test/java/pl/haladyj/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
