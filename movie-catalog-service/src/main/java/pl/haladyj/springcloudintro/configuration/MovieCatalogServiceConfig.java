cef31a4ba4c30842c26260fbe6a554fef3e5966a
Pawel Haladyj <pawel@haladyj.pl>
2020-04-01 14:57:22 +0200

    movie catalog service timeout -> config

--- movie-catalog-service/src/main/java/pl/haladyj/springcloudintro/configuration/MovieCatalogServiceConfig.java
+++ movie-catalog-service/src/main/java/pl/haladyj/springcloudintro/configuration/MovieCatalogServiceConfig.java
@@ -3,16 +3,29 @@ package pl.haladyj.springcloudintro.configuration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MovieCatalogServiceConfig {
 
-    @Bean
+/*    @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }*/
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        //connection timeout 3000ms = 3s
+        clientHttpRequestFactory.setConnectTimeout(3000);
+
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
     @Bean
